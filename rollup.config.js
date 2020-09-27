97f1a8d4dccab70ea6f00fb371b1d150b8d0a582
Paz H <pazz1001@walla.com>
2020-09-27 11:55:23 +0300

    updated rollup version

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,22 @@
-import buble from "rollup-plugin-buble"
+import buble from "@rollup/plugin-buble"
 
-export default {
-  entry: "src/index.js",
-  moduleName: "chavascript-parser",
-  plugins: [
-    buble({transforms: {dangerousForOf: true}})
-  ],
-  sourceMap: true,
-  targets: [
-    {dest: "dist/chavascript-parser.js", format: "umd"},
-    {dest: "dist/chavascript-parser.mjs", format: "es"}
-  ]
-}
+export default [
+  {
+    input: "src/index.js",
+    moduleName: "chavascript-parser",
+    plugins: [
+      buble({ transforms: { dangerousForOf: true } })
+    ],
+    sourceMap: true,
+    output: [
+      {
+        file: "dist/chavascript-parser.js",
+        formats: "umd"
+      },
+      {
+        file: "dist/chavascript-parser.mjs",
+        formats: "es"
+      }
+    ]
+  }
+]
